db33c8811949234e52efe8b9b049f6f62d5718b2
NiSHAN808 <nishan4b@gmail.com>
2025-06-15 21:51:05 +0545

    initial commit - just clearing react code and setuping tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+    
+
+  return (
+    <>
+<div className='bg-red-300'>hellow bro if you are seeing this that means the tailwindcss and react is working perfectly</div>
+    </>
+  )
+
+}
+export default App
